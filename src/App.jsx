a10702a24832691f24b20cc5d1e3b1f11bfbbd70
Ryota Mine <ryotamine@gmail.com>
2019-04-09 15:14:56 +0000

    set initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
-import ChatBar from './ChatBar.jsx';
-import Message from './Message.jsx';
 import MessageList from './MessageList.jsx';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
       <div>
