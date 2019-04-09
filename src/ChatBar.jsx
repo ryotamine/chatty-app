93fe59bc50f19069609b37beb19be5e50c5e9945
Ryota Mine <ryotamine@gmail.com>
2019-04-09 17:54:24 +0000

    add unique key prop

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -8,7 +8,7 @@ class ChatBar extends Component {
   render() {
     return (
       <footer className='chatbar'>
-        <input className='chatbar-username' placeholder='Your Name (Optional)' defaultValue={this.props.currentUser.name} />
+        <input className='chatbar-username' placeholder='Your Name (Optional)' defaultValue={this.props.currentUser} />
         <input className='chatbar-message' placeholder='Type a message and hit ENTER' />
       </footer>
     );
